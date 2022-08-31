a353da58e6556e7b8e4ee62d5196a7bad38a6b29
Martex1230#$ <adeagbothompson@gmail.com>
2022-08-31 10:57:17 +0100

    second

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:travel/widgets/app_large_text.dart';
+import 'package:travel/widgets/app_text.dart';
+import 'package:travel/widgets/responsive_button.dart';
+
+import '../misc/colors.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+
+  List texts = [
+    [
+      "Trips",
+      "Mountain",
+      "Mountain hikes give you an incredible sense of freedon along with endurance test"
+    ],
+    [
+      "2. Trips",
+      "2. Mountain",
+      "2. Mountain hikes give you an incredible sense of freedon along with endurance test"
+    ],
+    [
+      "3. Trips",
+      "3. Mountain",
+      "3. Mountain hikes give you an incredible sense of freedon along with endurance test"
+    ],
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "img/" + images[index],
+                      ),
+                      fit: BoxFit.cover)),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: texts[index][0]),
+                        AppText(text: texts[index][1], size: 30),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: 250,
+                          child: AppText(
+                            text: texts[index][2],
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ResponsiveButton(
+                          width: 120,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 2),
+                            width: 8,
+                            height: index == indexDots ? 25 : 8,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexDots ? AppColors.mainColor : AppColors.mainColor.withOpacity(0.3) 
+                            ),
+                          );
+                        })
+                      
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
